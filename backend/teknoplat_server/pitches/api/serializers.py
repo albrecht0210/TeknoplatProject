27acb34910834b1c6fd46306c92466861419a378
Albrecht Christian Lanojan <albrecht.lanojan0210@gmail.com>
2023-12-22 01:10:56 +0800

    Add team_json field

--- backend/teknoplat_server/pitches/api/serializers.py
+++ backend/teknoplat_server/pitches/api/serializers.py
@@ -1,7 +1,24 @@
+import json
+
 from rest_framework import serializers
 from ..models import Pitch
+from .external_services import fetch_team
 
 class PitchSerializer(serializers.ModelSerializer):
+    team_json = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Pitch
         fields = '__all__'
+
+    def get_team_json(self, obj):
+        team = fetch_team(obj.team, self.context.get('request'))
+        team_response = json.loads(team.content.decode('utf-8'))
+
+        if team.status_code == 404:
+            raise serializers.ValidationError(team_response)
+        elif team.status_code == 500:
+            raise serializers.ValidationError({'error': 'Team Management Server is down.'})
+
+        return team_response
+
