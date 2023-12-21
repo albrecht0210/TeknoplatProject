93d2ccd05bcfa51328c8f15948f949a3a8bc0d6d
Albrecht Christian Lanojan <albrecht.lanojan0210@gmail.com>
2023-12-22 01:13:15 +0800

    add new routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,10 @@
 import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import ErrorPage from "./features/error/ErrorPage";
-import LoginPage, { loginAction } from "./features/login/LoginPage";
-import MeetingsLayout from "./layouts/MeetingsLayout";
+import { action as loginAction } from "./features/login/LoginPage";
+import { action as meetingAction } from "./layouts/MeetingLayout";
+import { action as commentAction } from "./features/meeting/MeetingDetails.Comments";
+import { action as chatbotAction } from "./features/chatbot/ChatbotPage";
 
 const UrlPaths = () => {
     const routes = createRoutesFromElements(
@@ -21,25 +23,49 @@ const UrlPaths = () => {
                     />
                     <Route 
                         path="courses/" 
-                        element={<Navigate to="/" />}
+                        lazy={() => import("./features/redirects/CoursesRedirects")}
                     />
                     <Route 
                         path="courses/:courseId" 
-                        element={<Navigate to="courses/:courseId/meetings/" />}
+                        lazy={() => import("./features/redirects/CoursesRedirects")}
                     />
                     <Route 
                         path="courses/:courseId/meetings/" 
                         lazy={() => import("./layouts/MeetingsLayout")}
                         handle={{ crumb: () => <span>{localStorage.getItem("course_link_name")}</span> }}
                     >
-                        
+                        <Route 
+                            path=":status/"
+                            lazy={() => import("./features/meeting/MeetingList")}
+                        />
                     </Route>
-                    <Route path="courses/:courseId/meetings/:meetingId/" />
-                    <Route path="chatbot/" />
+                    <Route 
+                        path="courses/:courseId/meeting/" 
+                        lazy={() => import("./features/redirects/CoursesRedirects")}
+                    />
+                    <Route 
+                        path="courses/:courseId/meeting/:meetingId/"
+                        lazy={() => import("./layouts/MeetingLayout")}
+                        handle={{ crumb: () => <span>{localStorage.getItem("meeting_link_name")}</span> }}
+                        action={meetingAction}
+                    >
+                        <Route path="pitches/" lazy={() => import("./features/meeting/MeetingDetails.Pitches")} />
+                        <Route path="criterias/" lazy={() => import("./features/meeting/MeetingDetails.Criterias")} />
+                        <Route path="comments/" lazy={() => import("./features/meeting/MeetingDetails.Comments")} action={commentAction} />
+                    </Route>
+                    <Route 
+                        path="chatbot/" 
+                        lazy={() => import("./features/chatbot/ChatbotPage")} 
+                        action={chatbotAction}
+                    />
                 </Route>
-                <Route path="live/:meetingId/"/>
+                <Route path="live/:meetingId/" />
             </Route>
-            <Route path="login" element={<LoginPage />} action={loginAction} />
+            <Route 
+                path="login" 
+                lazy={() => import("./features/login/LoginPage")}
+                action={loginAction} 
+            />
         </Route>
     );
 }
